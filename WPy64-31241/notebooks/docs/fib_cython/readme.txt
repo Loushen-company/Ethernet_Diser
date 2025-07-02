609ebea8a0d69493ef5feebc778309a5d59302f1
MountainCompany <ertusavagewolf@gmail.com>
2025-07-02 22:21:48 +0300

    Ethernet_Diser

--- WPy64-31241/notebooks/docs/fib_cython/readme.txt
+++
@@ -1,9 +0,0 @@
-https://cython.readthedocs.io/en/latest/src/tutorial/cython_tutorial.html#fibonacci-fun
-
-cd here 
-
-
-python setup.py build_ext --inplace
-
-then start python and do
- "import fib"
--- optimizer.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def disable_services(service_list):
-    for service in service_list:
-        subprocess.call(f"sc stop {service}", shell=True)
-
-def enable_services(service_list):
-    for service in service_list:
-        subprocess.call(f"sc start {service}", shell=True)
